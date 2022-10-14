2cc360ce87ed60a622adb775c58b40baa0932d0a
abhijeetpode <106150468+abhijeet1122333@users.noreply.github.com>
2022-10-14 08:40:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { TodoInput } from './components/TodoInput';
+import { List } from './components/TodoList';
+import { Todo } from './components/todo';
+function App() {
+
+
+  return (
+    <div className="App">
+
+     <Todo></Todo>
+    </div>
+  );
+}
+
+export default App;
